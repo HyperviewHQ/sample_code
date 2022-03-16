6e815176adc037eaa32d9b84b933f63cb67dcd75
Rami Jebara <rami.jebara@hyperviewhq.com>
2022-03-15 20:39:26 -0700

    Added a script doc header

--- src/powershell/02-upload_floormounted_assets.ps1
+++ src/powershell/02-upload_floormounted_assets.ps1
@@ -1,3 +1,26 @@
+<#
+  .SYNOPSIS
+  Performs a simple upload of floor mounted assets.
+
+  .DESCRIPTION
+  Performs a simple upload of floor mounted assets. Uses a standard CSV import format.
+  Review example file for more information. Note that that import file uses internal IDs
+  for model information and asset type.
+
+  .PARAMETER InputPath
+  None. 
+
+  .PARAMETER OutputPath
+  None.
+
+  .INPUTS
+  Two configuration files. One for hostname and another for client credentials.
+  Data file in ./data/floor_mounted.csv.
+
+  .OUTPUTS
+  Status of uploads and any API error messages, where applicable.
+#>
+
 # Import asset helper functions
 Import-Module ./lib/asset_helpers.psm1
 
