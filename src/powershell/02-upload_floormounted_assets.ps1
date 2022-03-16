347e3a3f51e7589d0804c8c3f9e8cbfca6f7d049
Rami Jebara <rami.jebara@hyperviewhq.com>
2022-03-15 20:32:58 -0700

    initial upload asset example

---
+++ src/powershell/02-upload_floormounted_assets.ps1
@@ -0,0 +1,68 @@
+# Import asset helper functions
+Import-Module ./lib/asset_helpers.psm1
+
+# Read Client Configuration
+$ClientConfiguration = Get-Content -Raw -Path ./conf/client_credential.json | ConvertFrom-Json
+
+# Read Hostname
+$HyperviewHost = Get-Content -Raw -Path ./conf/hostname.json | ConvertFrom-Json
+
+#Fetch access token.
+$PayloadBody = @{
+    grant_type    = "client_credentials"
+    client_id     = $ClientConfiguration.ClientId
+    client_secret = $ClientConfiguration.ClientSecret
+};
+
+# Put your Hyperview hostname here
+$HostName = $HyperviewHost.Hostname;
+$TokenUrl = [string]::Format("https://{0}/connect/token", $HostName);
+
+$FetchTokenHeaders = @{
+    "Content-Type" = "application/x-www-form-urlencoded"
+}
+
+try {
+    $resp = Invoke-RestMethod -Method Post -Headers $FetchTokenHeaders -Body $PayloadBody -Uri $TokenUrl
+    Write-Verbose "Successfully authenticated...";
+    $accessToken = $resp.access_token;
+}
+catch {
+    Write-Output "Failed to authenticate. Exiting...";
+    Exit $LASTEXITCODE;
+}
+
+# Read CSV File
+$CsvData = Import-Csv -Path ./data/floor_mounted.csv
+
+# Start Upload Loop
+foreach ($line in $CsvData) {
+    $LocationId = Get-LocationId -AccessToken $accessToken -LocationId $line.Location -ApiHost $HostName;
+
+    $AssetObject = @{
+        "name"                     = $line.Name;
+        "status"                   = 0; # Hardcode asset status id
+        "assetTypeId"              = $line.AssetType;
+        "parentId"                 = $LocationId;
+        "creatableAssetProperties" = @(
+            @{
+                "type"  = 1;
+                "value" = $line.SerialNumber;
+            }
+        );
+        "assetLifecycleState"      = 0;
+        "productId"                = $line.ModelId;
+    };
+
+    # Add asset tracker id if it is in the upload file
+    if (-not ([string]::IsNullOrEmpty($line.AssetTrackerId))) {
+        $AssetObject.creatableAssetProperties += @{
+            "type"  = 147;
+            "value" = $line.AssetTrackerId;
+        }
+    }
+
+    Write-Host "Creating Asset: " $line.Name " Location: " $line.Location;
+
+    Add-Asset -AssetObject $AssetObject -ApiHost $HostName ;
+}
---
+++ src/powershell/lib/asset_helpers.psm1
@@ -0,0 +1,126 @@
+<#
+  .DESCRIPTION
+  A collection of asset API helper functions
+#>
+
+# Get Rack Id for an asset using advanced search API
+function Get-LocationId {
+    param (
+        $AccessToken,
+        $LocationId,
+        $ApiHost
+    )
+
+    $SearchUri = [string]::Format("https://{0}/api/asset/search", $ApiHost);
+
+    $LocationData = $LocationId.Split("/")
+    $EndLocation = $LocationData[-1]
+    $LocationPath = $LocationData[0..($LocationData.Length - 2)]
+    $LocationPathTabbed = $LocationPath -Join "`t"
+
+    $Headers = @{
+        "method"          = "POST";
+        "Content-Type"    = "application/json";
+        "Authorization"   = "Bearer $AccessToken";
+        "scheme"          = "https";
+        "path"            = "/api/asset/search";
+        "pragma"          = "no-cache";
+        "cache-control"   = "no-cache";
+        "accept"          = "application/json, text/plain, */*";
+        "origin"          = "$ApiHost";
+        "sec-fetch-site"  = "same-origin";
+        "sec-fetch-mode"  = "cors";
+        "sec-fetch-dest"  = "empty";
+        "accept-encoding" = "gzip, deflate, br";
+        "accept-language" = "en-US,en;q=0.9";
+    };
+
+    $SearchPayload = @{
+        "from"                    = "0";
+        "size"                    = "10";
+        "selectedFields"          = @("DisplayName");
+        "searchComplexDataFields" = @();
+        "query"                   = @{
+            "bool" = @{
+                "filter"               = @{
+                    "bool" = @{
+                        "must" = @(
+                            @{
+                                "match" = @{
+                                    "assetType" = "Location";
+                                };
+                            },
+                            @{
+                                "wildcard" = @{
+                                    "tabDelimitedPath" = "$LocationPathTabbed*";
+                                };
+                            }
+                        );
+                    };
+                };
+                "should"               = @(
+                    @{
+                        "query_string" = @{
+                            "query"  = "`"$EndLocation`"";
+                            "fields" = @(
+                                "displayNameLowerCase^5";
+                                "*";
+                            );
+                        };
+                    }
+                );
+                "minimum_should_match" = 1;
+            };
+        };
+    };
+
+    $Response = Invoke-WebRequest -Uri $SearchUri -Method "POST" `
+        -Headers $Headers `
+        -ContentType "application/json" `
+        -Body ($SearchPayload | ConvertTo-Json -Depth 9) |
+    ConvertFrom-Json |
+    Select-Object -Property data;
+
+    $AssetData = $Response.data
+
+    if ($Response.data.length -gt 1) {
+        $AssetData = $Response.data | Where-Object { $_.displayName -eq $RackName }
+    }
+
+    $RackId = $AssetData.id;
+
+    return $RackId;
+}
+
+# Add Asset
+function Add-Asset {
+    param (
+        $AssetObject,
+        $ApiHost
+    )
+
+    $AddAssetUrl = [string]::Format("https://{0}/api/asset/assets", $ApiHost);
+
+    $Headers = @{
+        "method"          = "POST";
+        "Content-Type"    = "application/json";
+        "Authorization"   = "Bearer $AccessToken";
+        "scheme"          = "https";
+        "path"            = "/api/asset/search";
+        "pragma"          = "no-cache";
+        "cache-control"   = "no-cache";
+        "accept"          = "application/json, text/plain, */*";
+        "origin"          = "$ApiHost";
+        "sec-fetch-site"  = "same-origin";
+        "sec-fetch-mode"  = "cors";
+        "sec-fetch-dest"  = "empty";
+        "accept-encoding" = "gzip, deflate, br";
+        "accept-language" = "en-US,en;q=0.9";
+    };
+
+    Invoke-WebRequest -Uri $AddAssetUrl -Method "POST" `
+        -Headers $Headers `
+        -ContentType "application/json" `
+        -Body ($AssetObject | ConvertTo-Json) |
+    ConvertFrom-Json 
+}
