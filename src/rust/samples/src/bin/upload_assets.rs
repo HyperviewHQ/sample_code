3e0ce79af7b0744be3b308cb31ccdcb8436dd9d2
Rami Jebara <rami.jebara@hyperviewhq.com>
2022-04-10 17:50:31 -0700

    updated rust program to upload in_rack assets like servers

--- src/rust/samples/src/bin/upload_assets.rs
+++ src/rust/samples/src/bin/upload_assets.rs
@@ -24,6 +24,9 @@ struct Record {
     model_id: String,
     serial_number: String,
     asset_tag: String,
+    rack_u_location: Option<String>,
+    rack_side: Option<String>,
+    rack_position: Option<String>,
 }
 
 fn main() {
@@ -52,13 +55,24 @@ fn main() {
         for record in reader.deserialize() {
             let r: Record = record.unwrap();
 
-            let parent_id = get_parent_id(
-                &auth_header,
-                &hv_config.instance_url,
-                &"Location".to_string(),
-                &r.location,
-            );
+            // Find parent id
+            let parent_id = if r.asset_type == "rack" {
+                get_parent_id(
+                    &auth_header,
+                    &hv_config.instance_url,
+                    &"Location".to_string(),
+                    &r.location,
+                )
+            } else {
+                get_parent_id(
+                    &auth_header,
+                    &hv_config.instance_url,
+                    &"Rack".to_string(),
+                    &r.location,
+                )
+            };
 
+            // Compose the asset properties array
             let mut asset_properties: Vec<Value> = Vec::new();
 
             asset_properties.push(json!({
@@ -73,11 +87,25 @@ fn main() {
                 }));
             }
 
+            // Compose locationData object
+            let mut location_data = json!({ "parentId": parent_id.clone() });
+            if let Some(rack_side) = r.rack_side {
+                location_data["rackSide"] = Value::from(rack_side.to_lowercase());
+            }
+            if let Some(rack_u_location) = r.rack_u_location {
+                location_data["rackULocation"] = Value::from(rack_u_location.to_lowercase());
+            }
+            if let Some(rack_position) = r.rack_position {
+                location_data["rackPosition"] = Value::from(rack_position.to_lowercase());
+            }
+
+            // Reconcile asset lifecycle state
             let mut asset_lifecycle_state = "0".to_string();
             if !r.lifecycle_state.is_empty() {
                 asset_lifecycle_state = r.lifecycle_state.clone();
             }
 
+            // Compose asset object
             let asset = json!({
                 "name": r.name,
                 "status": 0,
@@ -85,10 +113,14 @@ fn main() {
                 "parentId": parent_id,
                 "assetLifecycleState": asset_lifecycle_state,
                 "productId": r.model_id,
-                "creatableAssetProperties": asset_properties
+                "creatableAssetProperties": asset_properties,
+                "locationData": location_data
             });
 
-            println!("{}", serde_json::to_string_pretty(&asset).unwrap());
+            println!(
+                "-------------------------\nPayload: {}\n",
+                serde_json::to_string_pretty(&asset).unwrap()
+            );
 
             // Instantiate a client
             let req = reqwest::blocking::Client::new();
@@ -105,7 +137,10 @@ fn main() {
                 .json::<Value>()
                 .unwrap();
 
-            println!("{}", serde_json::to_string_pretty(&resp).unwrap());
+            println!(
+                "Server Response: {}\n",
+                serde_json::to_string_pretty(&resp).unwrap()
+            );
         }
     } else {
         println!("Error: Could not open csv file.");
